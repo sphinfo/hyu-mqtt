1cf345a276c8cc10809fbdb1863ea48fb7e21841
AnByoungJun <bjan@sphinfo.co.kr>
2024-09-16 23:21:28 +0900

    [MQTT 구독] feat: 그레이더 MQTT 구독 feat: 롤러 MQTT 구독 refactor: 코드 정리

--- src/main/java/com/sph/hyu/mqtt/subscriber/grader/MqttGraderMessageSubscriber.java
+++ src/main/java/com/sph/hyu/mqtt/subscriber/grader/MqttGraderMessageSubscriber.java
@@ -1,5 +1,10 @@
 package com.sph.hyu.mqtt.subscriber.grader;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessagingException;
@@ -9,14 +14,32 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.sph.hyu.mqtt.comm.LocalDateTimeDeserializer;
+import com.sph.hyu.mqtt.subscriber.tracking.domain.TbIotItTrackingHist;
+import com.sph.hyu.mqtt.subscriber.tracking.repository.TrackingHistoryJDBCRepository;
+import com.sph.hyu.mqtt.subscriber.tracking.repository.TrackingHistoryRepository;
 
+@Component
 public class MqttGraderMessageSubscriber implements MessageHandler {
 
 	private final ObjectMapper objectMapper;
 	
+	private final ModelMapper modelMapper;
+	
+	@Autowired
+	private TrackingHistoryRepository trackingHistoryRepository;
+	
+	@Autowired
+	private TrackingHistoryJDBCRepository trackingHistoryJDBCRepository;
+	
 	public MqttGraderMessageSubscriber () {
         objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(new JavaTimeModule());
+        
+        modelMapper = new ModelMapper();
     }
 	
     @Override
@@ -26,8 +49,36 @@ public class MqttGraderMessageSubscriber implements MessageHandler {
         	
         	TrackingGraderHistoryDto dto = null;
         	
+        	TbIotItTrackingHist tbIotItTrackingHist = null;
+        	
         	try {
+        		// 사용자 정의 모듈 등록
+                SimpleModule module = new SimpleModule();
+                module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
+                objectMapper.registerModule(module);
+                
     			dto = objectMapper.readValue(payload, TrackingGraderHistoryDto.class);
+    			
+    			tbIotItTrackingHist = modelMapper.map(dto, TbIotItTrackingHist.class);
+            	
+            	String trackingId = trackingHistoryRepository.getTrackingId();
+            	tbIotItTrackingHist.setTrackingId(trackingId);
+            	
+            	String eventDate = dto.getEventDt().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+            	tbIotItTrackingHist.setEventDate(eventDate);
+            	
+            	LocalDateTime now = LocalDateTime.now();
+            	tbIotItTrackingHist.setRegDt(now);
+            	tbIotItTrackingHist.setRegId("MQTT");
+            	
+            	trackingHistoryRepository.addHist(tbIotItTrackingHist);
+            	
+            	trackingHistoryJDBCRepository.callMergeTrackingNow(
+            			tbIotItTrackingHist.getPrjId(),
+            			tbIotItTrackingHist.getAssetId(),
+            			tbIotItTrackingHist.getUserId(),
+            			tbIotItTrackingHist.getTrackingId()
+        			);
     		} catch (JsonMappingException e) {
     			// TODO Auto-generated catch block
     			e.printStackTrace();
--- src/main/java/com/sph/hyu/mqtt/subscriber/tracking/repository/TrackingHistoryJDBCRepository.java
+++ src/main/java/com/sph/hyu/mqtt/subscriber/tracking/repository/TrackingHistoryJDBCRepository.java
@@ -1,5 +1,6 @@
 package com.sph.hyu.mqtt.subscriber.tracking.repository;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.sph.hyu.mqtt.subscriber.tracking.domain.TbIotItTrackingHist;
@@ -23,19 +25,10 @@ public class TrackingHistoryJDBCRepository {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
+	@SuppressWarnings("deprecation")
 	public String callMergeTrackingNow(UUID prjId, UUID assetId, String userId, String trackingId) {
 		String sql = "SELECT iot.fn_merge_tracking_now(CAST(? AS uuid), CAST(? AS uuid), CAST(? AS varchar), CAST(? AS varchar))";
-
+		
 	    return jdbcTemplate.queryForObject(sql, new Object[]{prjId, assetId, userId, trackingId}, String.class);
-	    
-	    /*
-		jdbcTemplate.update("SELECT iot.fn_merge_tracking_now(CAST(? AS varchar), CAST(? AS varchar), CAST(? AS varchar), CAST(? AS varchar))",
-	                         prjId, assetId, userId, trackingId);
-*/
 	}
-
-	
-	//@Query(value = "SELECT iot.fn_merge_tracking_now(CAST(:prjId AS varchar), CAST(:assetId AS varchar), CAST(:userId AS varchar), CAST(:trackingId AS varchar))", nativeQuery = true)
-	//void mergeTrackingNow(@Param("prjId") String prjId, @Param("assetId") String assetId, @Param("userId") String userId, @Param("trackingId") String trackingId);
-
 }
